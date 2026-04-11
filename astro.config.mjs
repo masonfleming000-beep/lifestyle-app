2abd59eb865ba70d9dd38c0215027d461dd92ad8
flem5 <fleminn@purdue.edu>
2026-04-11 13:30:52 -0400

    upload secutriy try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 
 export default defineConfig({
+  site: "https://www.hublifeapp.com",
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
+  security: {
+    allowedDomains: [
+      { protocol: "https", hostname: "www.hublifeapp.com" },
+      { protocol: "https", hostname: "hublifeapp.com" },
+    ],
+  },
 });
